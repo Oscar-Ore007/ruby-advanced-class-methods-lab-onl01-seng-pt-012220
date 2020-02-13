05c9b0c762c4e9b203d2ea620b879037bb892e4f
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-13 02:23:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,8 +16,8 @@ def self.create
   song 
 end
 
-def self.new_by_name(title)
-  song = self.new 
-  song.name = title 
-  song 
-end 
+ def self.new_by_name(title) 
+    song = self.new
+    song.name = title
+    song
+  end
