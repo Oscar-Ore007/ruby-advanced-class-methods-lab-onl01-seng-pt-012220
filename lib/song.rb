9e61d8c0403f27c3dc60649148a2f0780f7c6a1d
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-13 02:38:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ def self.find_by_name(title)
   end
 
 def self.find_or_create_by_name
-  result = self.find_by_name(title)
+    result = self.find_by_name(title)
     if result
       result
     else
